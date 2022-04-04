e17269fe26aa42b83fd27024c54a8da6d61de0c7
HoldenLalumiere <holdenlalumiere@gmail.com>
2022-04-03 23:50:17 -0400

    Added prints for if the file is not a .hold or if it is not a .x

--- Holds/src/Hold.java
+++ Holds/src/Hold.java
@@ -8,22 +8,34 @@ public class Hold {
 
     /**
      * Read in a .hold file
+     *
+     * @param args the arguments from main, it should be 1 .hold file
      */
     public static void hold(String[] args){
         // Check if there is exactly 1 argument
         if (args.length < 1){
             System.err.println("Not enough arguments, try giving a .hold file");
+            System.exit(-1);
         }
         else if (args.length > 1){
             System.err.println("Too many arguments, try giving one .hold file");
+            System.exit(-1);
         }
 
+        System.out.println(args[0]);
+        String file = args[0];
+        String[] splitFile = file.split("\\.");
+        if (splitFile.length != 2) {
+            System.err.println("File is not a .hold, try giving a .hold file");
+            System.exit(-1);
+        }
 
-
-
-
-
-
+        String holdCheck = splitFile[1];
+        System.out.println(holdCheck);
+        if (!holdCheck.equals("hold")){
+            System.err.println("File is not a .hold, try giving a .hold file");
+            System.exit(-1);
+        }
 
 
 
