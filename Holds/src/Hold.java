7732d23420c0058b27b8a16ca306c909129272aa
HoldenLalumiere <holdenlalumiere@gmail.com>
2022-04-03 23:30:56 -0400

    added the main function and prints errors if there are too many or too little files

--- Holds/src/Hold.java
+++ Holds/src/Hold.java
@@ -1,6 +1,54 @@
 public class Hold {
+    /**
+     * Run the program
+     */
+    public static void main(String[] args){
+        hold(args);
+    }
+
+    /**
+     * Read in a .hold file
+     */
+    public static void hold(String[] args){
+        // Check if there is exactly 1 argument
+        if (args.length < 1){
+            System.err.println("Not enough arguments, try giving a .hold file");
+        }
+        else if (args.length > 1){
+            System.err.println("Too many arguments, try giving one .hold file");
+        }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
